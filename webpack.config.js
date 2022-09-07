3b92ae23a16b0ac70d3325d29317ddd416d00667
pavlos noulis <pavlos.noulis@gmail.com>
2022-09-08 01:49:58 +0300

    Added ability to draw configuration from envars through dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 import { join, parse } from 'node:path';
+import Dotenv from 'dotenv-webpack';
 
 const
 projectRoot = parse(new URL(import.meta.url).pathname).dir
 
 export default {
-    mode: 'development',
+    mode: `${process.env.NODE_ENV}`,
     entry: {
         main: join(projectRoot, 'src', 'index.js')
     },
@@ -16,5 +17,17 @@ export default {
     resolve: {
         modules: ['node_modules'],
         preferRelative: false
-    }
+    },
+    plugins: [
+        new Dotenv({
+            // 1st be safe
+            safe: join(projectRoot, 'config', '.env.example'),
+            // 2nd load defaults
+            defaults: join(projectRoot, 'config', '.env.dev.defaults'),
+            // 3rd load secrets
+            path: join(projectRoot, 'config', '.env.dev'),
+            // 4th CL arguments trample all
+            systemvars: true
+        })
+    ]
 }
