f41db54831a04614091a19d411e5337e064ab294
pavlos noulis <pavlos.noulis@gmail.com>
2022-09-08 16:07:09 +0300

    Middleware can optionally be added according to development environment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import services from './routes.js';
 const app = new Koa();
 const router = new Router();
 
+if (process.env.NODE_ENV === 'development') {
+    const { default: PrettyJson } = await import('koa-json');
+    app.use(PrettyJson({ pretty: true, spaces: 2 }));
+}
+
 router.get('/', async (ctx, next) => {
-    console.log(services.stack.map($_ => $_.path))
+    ctx.response.body = router.stack.map($_ => $_.path)
 });
 router.use(services.routes());
 
