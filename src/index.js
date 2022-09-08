648c30b3d97d1ffcfa5b44e8703472a17d8250f5
pavlos noulis <pavlos.noulis@gmail.com>
2022-09-08 16:22:24 +0300

    added koa-body for parsing request bodies; most services now utilize the post method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Koa from 'koa';
 import Router from '@koa/router';
+import KoaBody from 'koa-body';
 import services from './routes.js';
+import koaBody from 'koa-body';
 
 const app = new Koa();
 const router = new Router();
 
+app.use(koaBody())
 if (process.env.NODE_ENV === 'development') {
     const { default: PrettyJson } = await import('koa-json');
     app.use(PrettyJson({ pretty: true, spaces: 2 }));
