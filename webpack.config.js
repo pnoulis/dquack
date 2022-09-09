3a4c89e644c552182e8c9cae5c7c1756edc484bd
pavlos noulis <pavlos.noulis@gmail.com>
2022-09-09 09:49:28 +0300

    Removed code induced by the need to develop locally in a windows machine; app now runs only on linux or wsl

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,16 @@
-import { join, parse } from 'node:path';
+import { resolve, parse } from 'node:path';
 import Dotenv from 'dotenv-webpack';
 
-let projectRoot = ''
-switch (process.platform) {
-    case 'win32':
-        projectRoot = parse(new URL(import.meta.url).pathname).dir.substring(1,)
-        break;
-    case 'linux':
-        projectRoot = parse(new URL(import.meta.url).pathname).dir
-        break;
-    default:
-        throw `Unrecognized platform: ${process.platform}`
-}
+const projectRoot = parse(new URL(import.meta.url).pathname).dir
 
 export default {
     mode: `${process.env.NODE_ENV}`,
     entry: {
-        main: join(projectRoot, 'src', 'index.js')
+        main: resolve(projectRoot, 'src', 'index.js')
     },
     output: {
         filename: '[name].bundle.cjs',
-        path: join(projectRoot, 'build'),
+        path: resolve(projectRoot, 'build'),
         clean: true
     },
     resolve: {
@@ -30,11 +20,11 @@ export default {
     plugins: [
         new Dotenv({
             // 1st be safe
-            safe: join(projectRoot, 'config', '.env.example'),
+            safe: resolve(projectRoot, 'config', '.env.example'),
             // 2nd load defaults
-            defaults: join(projectRoot, 'config', '.env.dev.defaults'),
+            defaults: resolve(projectRoot, 'config', '.env.dev.defaults'),
             // 3rd load secrets
-            path: join(projectRoot, 'config', '.env.dev'),
+            path: resolve(projectRoot, 'config', '.env.dev'),
             // 4th CL arguments trample all
             systemvars: true
         })
