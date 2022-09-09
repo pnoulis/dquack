d4a3d7fc25d14db020bf02cf8109f74b2abc41d0
pavlos noulis <pavlos.noulis@gmail.com>
2022-09-09 18:11:41 +0300

    installed standalone nodemon to check if i can tap into its std{in|out|err}

--- src/index.js
+++ src/index.js
@@ -8,18 +8,17 @@ const router = new Router();
 
 app.use(koaBody())
 if (process.env.NODE_ENV === 'development') {
-    const { default: PrettyJson } = await import('koa-json');
-    app.use(PrettyJson({ pretty: true, spaces: 2 }));
+  const { default: PrettyJson } = await import('koa-json');
+  app.use(PrettyJson({ pretty: true, spaces: 2 }));
 }
 
 router.get('/', async (ctx, next) => {
-    ctx.response.body = router.stack.map($_ => $_.path)
+  ctx.response.body = router.stack.map($_ => $_.path)
 });
 router.use(services.routes(), services.allowedMethods());
 
 app.use(
-    router.routes(),
-    router.allowedMethods(),
+  router.routes(),
+  router.allowedMethods(),
 );
 app.listen(8080); // 80 thousand
-// some etuht
--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,42 @@ import DotenvPlugin from 'dotenv-webpack';
 const projectRoot = parse(new URL(import.meta.url).pathname).dir
 
 export default {
-    mode: `${process.env.NODE_ENV}`,
-    entry: {
-        main: resolve(projectRoot, 'src', 'index.js')
-    },
-    output: {
-        filename: '[name].bundle.cjs',
-        path: resolve(projectRoot, 'build'),
-        clean: true
-    },
-    resolve: {
-        modules: ['node_modules'],
-        preferRelative: false
-    },
-    plugins: [
-        new DotenvPlugin({
-            // 1st be safe
-            safe: resolve(projectRoot, 'config', '.env.example'),
-            // 2nd load defaults
-            defaults: resolve(projectRoot, 'config', '.env.dev.defaults'),
-            // 3rd load secrets
-            path: resolve(projectRoot, 'config', '.env.dev'),
-            // 4th CL arguments trample all
-            systemvars: true
-        }),
-        new NodemonPlugin({
-          watch: resolve(projectRoot, 'build'),
-          runOnChangeOnly: false,
-          events: {
-            "start": "echo start",
-            "restart": "echo restart",
-          }
-        })
-    ],
-    experiments: {
-        topLevelAwait: true
-    }
+  mode: `${process.env.NODE_ENV}`,
+  entry: {
+    main: resolve(projectRoot, 'src', 'index.js')
+  },
+  output: {
+    filename: '[name].bundle.cjs',
+    path: resolve(projectRoot, 'build'),
+    clean: true
+  },
+  resolve: {
+    modules: ['node_modules'],
+    preferRelative: false
+  },
+  plugins: [
+    new DotenvPlugin({
+      // 1st be safe
+      safe: resolve(projectRoot, 'config', '.env.example'),
+      // 2nd load defaults
+      defaults: resolve(projectRoot, 'config', '.env.dev.defaults'),
+      // 3rd load secrets
+      path: resolve(projectRoot, 'config', '.env.dev'),
+      // 4th CL arguments trample all
+      systemvars: true
+    }),
+    new NodemonPlugin({
+      watch: resolve(projectRoot, 'build'),
+      runOnChangeOnly: false,
+      stdout: false,
+      events: {
+        "start": "echo start",
+        "restart": "echo restart",
+        "stdout": "echo stdout"
+      }
+    })
+  ],
+  experiments: {
+    topLevelAwait: true
+  }
 }
